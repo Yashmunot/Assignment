a1e0d82371d8e0188920d7f5e23f9e4d9b68ed94
Yashmunot <75272774+Yashmunot@users.noreply.github.com>
2020-12-01 10:45:57 +0530

    Add files via upload

---
+++ Assignment3/Assignment3/Bank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Bank
+    {
+        double NetBalance = 650000;
+        double transamount;
+
+        public void deposit()
+        {
+            NetBalance = NetBalance + transamount;
+            Console.WriteLine(NetBalance);
+        }
+        public void withdrawal()
+        {
+            NetBalance = NetBalance - transamount;
+            Console.WriteLine(NetBalance);
+        }   
+        public Bank(double transamount)
+        {
+            this.transamount = transamount;
+        }
+        public double Netbalance
+        {
+            get; set;
+        }
+    }
+    
+}
